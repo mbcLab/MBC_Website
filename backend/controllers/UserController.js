5ab169e39eef0fbb2bfbe15b5a9062529c947f86
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-03-12 19:25:38 +0700

    Fixing mongodb problem and creating new schema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,150 +1,208 @@
 import User from "../models/UserModel.js";
 import Project from "../models/ProjectModel.js";
 import Berita from "../models/BeritaModel.js";
- 
+import schema from "../models/schema.js";
+import { model } from "mongoose";
+
 // R
 export const getBerita = async (req, res) => {
-    try {
-        const Berita1 = await Berita.find();
-        res.json(Berita1);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
+  try {
+    const Berita1 = await Berita.find();
+    res.json(Berita1);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 export const getProject = async (req, res) => {
-    try {
-        const Project1 = await Project.find();
-        res.json(Project1);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
+  try {
+    const Project1 = await Project.find();
+    res.json(Project1);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 export const getUser = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // U
 export const updateProject = async (req, res) => {
-    try {
-        const updateProject = await Project.updateOne({_id:req.params.id}, {$set: req.body});
-        res.status(200).json(updateProject);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+  try {
+    const updateProject = await Project.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).json(updateProject);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 export const updateUser = async (req, res) => {
-    try {
-        const updateduser = await User.updateOne({_id:req.params.id}, {$set: req.body});
-        res.status(200).json(updateduser);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+  try {
+    const updateduser = await User.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).json(updateduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 export const updateBerita = async (req, res) => {
-    try {
-        const updateBerita = await Berita.updateOne({_id:req.params.id}, {$set: req.body});
-        res.status(200).json(updateBerita);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+  try {
+    const updateBerita = await Berita.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).json(updateBerita);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 // C
 
 export const saveBerita = async (req, res) => {
-    const Berita1 = new Berita(req.body);
-    try {
-        const insertedBerita = await Berita1.save();
-        res.status(201).json(insertedBerita);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+  // const Berita1 = new Berita(req.body);
+  const body = req.body;
+  const addList = {
+    title: body.judul,
+    date: Date.now(),
+    content: body.isi,
+  };
+  const berita = (await schema.findOne().exec()) ?? false;
+
+  if (!berita) {
+    return res.status(400).json({ message: "Unknown database." });
+  } else {
+    console.log("[*] Pushing to mongodb.");
+    berita.blogs.push(addList);
+    await berita.save();
+  }
+
+  try {
+    // const insertedBerita = await Berita1.save();
+    res.status(201).json(berita);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
 export const saveUser = async (req, res) => {
-    const User1 = new User(req.body);
-    try {
-        const insertedUser = await User1.save();
-        res.status(201).json(insertedUser);
-    } catch (error) {
-        res.status(400).json({message: error.message});
+  // const User1 = new User(req.body);
+  const body = req.body;
+  const list = {
+    name: body.nama,
+    social: body.social,
+    photo: "none", // need improvement on this line of code
+  };
+  const user = (await schema.findOne().exec()) ?? false;
+  if (!user) {
+    return res.status(400).json({ message: "No database entry." });
+  } else {
+    switch (body.divisi) {
+      case "Cyber":
+        user.users.cyber.push(list);
+        user.save();
+        break;
+      case "Gametech":
+        user.users.gametech.push(list);
+        user.save();
+        break;
+      case "GIS":
+        user.users.gis.push(list);
+        user.save();
+        break;
+      case "Big Data":
+        user.users.bigdata.push(list);
+        user.save();
+        break;
+
+      default:
+        console.log("404 Not Found\nSwitch case statement.");
+        break;
     }
-}
+  }
+  try {
+    // const insertedUser = await User1.save();
+    res.status(201).json(list);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 export const saveProject = async (req, res) => {
-    const Project1 = new Project(req.body);
-    try {
-        const inserteduser = await Project1.save();
-        res.status(201).json(inserteduser);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+  // const Project1 = new Project(req.body);
+  const proj = (await schema.findOne().exec()) ?? false;
+  if (!proj) {
+    return res.status(400).json({ message: "[?] Database not found." });
+  }
+  await proj.projects.push(req.body);
+  await proj.save();
+  return res.status(201).json({ message: "Project saved successfully." });
+};
 
- 
+// R
 
-// R 
- 
 export const getProjectById = async (req, res) => {
-    try {
-        const project = await Project.findById(req.params.id);
-        res.json(project);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-}
+  try {
+    const project = await Project.findById(req.params.id);
+    res.json(project);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 export const getProjectBydate = async (req, res) => {
-    try {
-        const project = await Project.findOne({tanggal:Date.now()});
-        res.json(project);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-}
+  try {
+    const project = await Project.findOne({ tanggal: Date.now() });
+    res.json(project);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 export const getBeritaById = async (req, res) => {
-    try {
-        const berita = await Berita.findById(req.params.id);
-        res.json(berita);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-}
+  try {
+    const berita = await Berita.findById(req.params.id);
+    res.json(berita);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 export const getUserById = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        res.json(user);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-}
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 // D
 export const deleteUser = async (req, res) => {
-    try {
-        const deleteduser = await User.deleteOne({_id:req.params.id});
-        res.status(200).json(deleteduser);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+  try {
+    const deleteduser = await User.deleteOne({ _id: req.params.id });
+    res.status(200).json(deleteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 export const deleteProject = async (req, res) => {
-    try {
-        const deleteduser = await Project.deleteOne({_id:req.params.id});
-        res.status(200).json(deleteduser);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+  try {
+    const deleteduser = await Project.deleteOne({ _id: req.params.id });
+    res.status(200).json(deleteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 export const deleteBerita = async (req, res) => {
-    try {
-        const deleteduser = await Berita.deleteOne({_id:req.params.id});
-        res.status(200).json(deleteduser);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
-
-
+  try {
+    const deleteduser = await Berita.deleteOne({ _id: req.params.id });
+    res.status(200).json(deleteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,31 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
- 
+import { config } from "dotenv";
+import schema from "./models/schema.js";
+
+config();
+
 const app = express();
-mongoose.connect('mongodb+srv://laboratoriumMBC:weattackweprotect23@labmbc.2vpngs6.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.mongodb, {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on('error', (error) => console.log(error));
-db.once('open', () => console.log('Database Connected...'));
- 
+db.on("error", (error) => console.log(error));
+db.once("open", async () => {
+  const check = (await schema.findOne().exec()) ?? false;
+  if (!check) {
+    console.log("[!] Creating new database entry.");
+    new schema().save();
+  }
+  console.log("Database Connected...");
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
- 
-app.listen(5000, ()=> console.log('Server up and running...'));
+
+app.listen(5000, () => console.log("Server up and running..."));
