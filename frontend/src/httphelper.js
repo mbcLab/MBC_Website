6cf854a00f30acfd3b2b2c6b14a0c6a28396ce6e
aji sasongko <57887588+jisong710@users.noreply.github.com>
2023-03-24 16:09:06 +0700

    Update httphelper.js

--- frontend/src/httphelper.js
+++ frontend/src/httphelper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/',
   headers: {
     "Content-type": "application/json"
   }
